94388b8638585b72cde5f8abb9ae4c658375d372
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-03-15 16:39:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
-require_relative 'config/environment'
+ require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello World" 
+  end
+  
+  get '/name' do 
+    "My name is Lucy" 
+  end
+  
+  get '/hometown' do 
+   "My hometown is Miami Beach"
+ end
+ 
+  get '/favorite-song' do 
+    "My favorite song is Hello"
+  end
+ 
 end
+
+
